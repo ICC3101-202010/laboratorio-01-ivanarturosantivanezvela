3438311820344939dbd7a7b52f76ec96d26762ae
Ivan Arturo Santivañez Vela <Ivan@MacBook-Pro-de-Clementina-757.local>
2020-03-21 17:27:29 -0300

    first commit

---
+++ Laboratorio1ivan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio1ivan
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
